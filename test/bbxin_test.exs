74c1e961b9cb6724fcd459a7dab6bf5b33b36fa9
Thomas Graves <n0t4d0ct0r@gmail.com>
2017-05-20 14:38:31 +0000

    Remove psuedocode

--- test/bbxin_test.exs
+++ test/bbxin_test.exs
@@ -2,11 +2,7 @@ defmodule BbxinTest do
   use ExUnit.Case
   doctest Bbxin
 
-  @token "MzE0OTc2NzczMDM5MzI1MTg0.DAAg-w.X79DewfXPy2ehZ2xOJt_AQ0qx7o"
-
   test "bbxin can prove she hear me" do
-    Bbxin.start(@token)
-    #|> Bbxin.listen(%{guild: @guild, channel: @channel})
-    #|> Bbxin.say("i'm sad and mean")
+    assert 1 + 1 == 2
   end
 end
