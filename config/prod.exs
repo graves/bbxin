505d97c2a6d25f7728005d9dd8520ab29c813739
Thomas Graves <n0t4d0ct0r@gmail.com>
2017-05-21 23:24:29 +0000

    Update configuration to move username and names to environment

--- config/prod.exs
+++ config/prod.exs
@@ -3,10 +3,10 @@ use Mix.Config
 config :bbxin, [
   discord_opts: %{
     token: System.get_env("BBXIN_TOKEN"),
-    guild: System.get_env("BBXIN_GUILD"),
-    channel: System.get_env("BBXIN_CHANNEL"),
+    guild: System.get_env("BBXIN_GUILD") || "",
+    channel: System.get_env("BBXIN_CHANNEL") || "",
     handler: Bbxin.Handler
   },
-  username: "xintani",
-  names: ["xintani", "xin", "xinxin"]
+  username: System.get_env("BBXIN_USERNAME"),
+  names: System.get_env("BBXIN_NAMES") |> String.split(",")
 ]
